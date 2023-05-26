c52906618591dbc961d967c46aacdab9f973cd61
Noah <noahmwangi@gmail.com>
2023-05-26 09:54:49 +0300

    Completed Assignment

--- lib/student.rb
+++ lib/student.rb
@@ -1,3 +1,21 @@
-class Student 
+require_relative './user'
 
+class Student < User
+  attr_accessor :knowledge
+
+  def initialize(first_name, last_name)
+    super(first_name, last_name)
+    @knowledge = []
+  end
+  
+    def learn(string)
+      @knowledge << string
+    end
+  
+    def knowledge
+      @knowledge
+    end
 end
+
+student = Student.new("John", "Doe")
+
